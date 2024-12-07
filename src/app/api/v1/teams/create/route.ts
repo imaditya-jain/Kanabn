8594fe7ef081e7a1e7477bb8df12f645f586b020
adityajain-web <adityaburse333@gmail.com>
2024-12-07 13:32:54 +0530

    empty

--- src/app/api/v1/teams/create/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import connectToDatabase from "@/src/config/db.config";
-import Team from "@/src/models/team.model";
-import User from "@/src/models/user.model";
-import { authMiddleware } from "@/src/middleware/auth.middleware";
-
-export async function POST(req: Request) {
-
-}
